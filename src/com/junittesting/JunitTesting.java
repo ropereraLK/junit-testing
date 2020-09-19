7f258d7f9d5e1d9818e05dbf590cfb89295c801e
Rohan Perera <ropereralk@gmail.com>
2020-09-19 18:04:18 +0530

    Basic test completed

--- src/com/junittesting/JunitTesting.java
+++ src/com/junittesting/JunitTesting.java
@@ -1,9 +1,12 @@
 package com.junittesting;
 
+import com.junittesting.testing.TestCalculator;
+
 public class JunitTesting {
 
 	public static void main(String[] args) {
-		
+		TestCalculator test = new TestCalculator();
+		test.testAdd();
 
 	}
 
--- src/com/junittesting/testing/TestCalculator.java
+++ src/com/junittesting/testing/TestCalculator.java
@@ -2,4 +2,15 @@ package com.junittesting.testing;
 
 public class TestCalculator {
 
+	public void testAdd() {
+
+		Calculator calculator = new Calculator();
+		
+		double result = calculator.add(25,13);
+		
+		if (result != 39.0) {
+			throw new RuntimeException("Expected 39.0; Result " + result);
+		}
+		
+	}
 }
